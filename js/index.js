67e01ad48c8d8083358f07fd6c2dbad968aa4a80
joshsj89 <joshsj89@gmail.com>
2023-09-03 06:56:28 -0400

    Now fetches APK versions

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
+    await getApks();
+
+    const apkButtons = document.querySelectorAll('.dropdown-item');
+    
+    
+    apkButtons.forEach((apkButton) => {
+        apkButton.addEventListener('click', handleClick);
+    });
+
     const apkButton = document.getElementById('apk-button');
-
-
-
-
-    apkButton.addEventListener('click', handleClick);
-})
-
+    const dropdown = document.getElementById('version-dropdown');
+    apkButton.addEventListener('click', () => dropdown.classList.toggle('show'));
+});
 
 const handleClick = async (e) => {
     e.preventDefault();
-    const downloadID = e.target.getAttribute('download-id');
+    
+    const apkButton = document.getElementById('apk-button');
+    const downloadID = apkButton.getAttribute('download-id');
+    const downloadID2 = e.target.getAttribute('download-id');
+    
     const options = {
         method: 'GET',
         headers: {
@@ -19,8 +28,8 @@ const handleClick = async (e) => {
     }
 
     try {
-        const responseInfo = await fetch(`https://joshsj89-1d7a9e7057c7.herokuapp.com/api/apps/${downloadID}`, options);
-        const response = await fetch(`https://joshsj89-1d7a9e7057c7.herokuapp.com/api/apps/download/${downloadID}`, options);
+        const responseInfo = await fetch(`https://joshsj89-1d7a9e7057c7.herokuapp.com/api/apps/${downloadID}/${downloadID2}`, options);
+        const response = await fetch(`https://joshsj89-1d7a9e7057c7.herokuapp.com/api/apps/download/${downloadID}/${downloadID2}`, options);
 
         if (!response.ok || !responseInfo.ok) {
             throw new Error('Network response was not ok');
@@ -36,5 +45,36 @@ const handleClick = async (e) => {
         link.click();
     } catch (error) {
         console.error('Error Downloading App: ', error);
+    }
+}
+
+const getApks = async () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const response = await fetch('https://joshsj89-1d7a9e7057c7.herokuapp.com/api/apps/versions/descending/64f3edf1d11932398c0807ee', options);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        const apks = data.apk;
+
+        apks.forEach((apk) => {
+            const dropdown = document.getElementById('version-dropdown');
+            const button = document.createElement('button');
+            button.setAttribute('class', 'dropdown-item');
+            button.setAttribute('download-id', apk._id);
+            button.textContent = apk.version;
+            dropdown.appendChild(button);
+        });
+    } catch (error) {
+        console.error('Error Fetching Apps: ', error);
     }
 }
